fb474fbcfb11f7f7618ce82320b24c66b450e7bd
Oscargil80 <oscargil80@gmail.com>
2024-05-18 00:43:30 -0400

    Aqui hay barra baja de navegacion, en el segundo fragment hya los diferentes toast y en el tercero hay una behavior botton Sheep

--- app/src/main/java/com/oscargil80/bottoncodelia/segundoFragment.kt
+++ app/src/main/java/com/oscargil80/bottoncodelia/segundoFragment.kt
@@ -1,59 +1,86 @@
 package com.oscargil80.bottoncodelia
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.oscargil80.bottoncodelia.databinding.FragmentSegundoBinding
+import www.sanju.motiontoast.MotionToast
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [segundoFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class segundoFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding:FragmentSegundoBinding? = null
+    private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_segundo, container, false)
+    ): View {
+      _binding = FragmentSegundoBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment segundoFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            segundoFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnMotionToast.setOnClickListener {
+            MotionToast.createToast(
+                requireContext() as Activity, // o requireActivity()
+                "Operacion Exitosa",
+                "Operacion Realizada  de forma exitosa",
+                MotionToast.TOAST_SUCCESS,
+                MotionToast.GRAVITY_CENTER,
+                MotionToast.SHORT_DURATION,
+                null
+            )
+
+            binding.btnColorMotionToast.setOnClickListener {
+                MotionToast.createToast(
+                    requireContext() as Activity, // o requireActivity()
+                    "Operacion Exitosa",
+                    "Operacion Realizada  de forma exitosa",
+                    MotionToast.TOAST_WARNING,
+                    MotionToast.GRAVITY_CENTER,
+                    MotionToast.SHORT_DURATION,
+                    null
+                )
             }
+
+            binding.btnDarkToast.setOnClickListener {
+                MotionToast.createToast(
+                    requireContext() as Activity, // o requireActivity()
+                    "Operacion Exitosa",
+                    "Operacion Realizada  de forma exitosa",
+                    MotionToast.TOAST_WARNING,
+                    MotionToast.GRAVITY_CENTER,
+                    MotionToast.SHORT_DURATION,
+                    null
+                )
+            }
+
+            binding.btnDarkColorToast.setOnClickListener {
+                MotionToast.createToast(
+                    requireContext() as Activity, // o requireActivity()
+                    "Operacion Exitosa",
+                    "Operacion Realizada  de forma exitosa",
+                    MotionToast.TOAST_WARNING,
+                    MotionToast.GRAVITY_CENTER,
+                    MotionToast.SHORT_DURATION,
+                    null
+                )
+            }
+
+
+
+
+
+        }
+
     }
 }
+
+// Toast.makeText(requireContext(), "Hola", Toast.LENGTH_LONG).show()
+//requireActivity(), en el this o primer parametro se puede poner tambien
